33be049deea760e6bc16623f67ad7b71fd6424c2
André Luiz Girol <andregirol@gmail.com>
2019-01-23 17:20:39 -0200

    Header for the first Dreamhost Script
    Starting with the "Shell Script Profissional" book

---
+++ dh_php_cli.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# WIP
+#
+# dh_php_cli.sh - Sets the PHP Cli to one available in Dreamhost's Shared Hosting
+#
+# Website: https://github.com/eluminatis
+# Author: André Luiz Girol <girol.dev@gmail.com>
+#
+# ------------------------------------------------------------------
+# TODO: ADD A NICE DESCRIPTION HERE
